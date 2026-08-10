b98dac351f0de3f5c44327a9135a07f6e67bffc0
ImMiJin <dlaalwls48@gmail.com>
2026-08-10 09:21:16 +0900

    Expo TypeScript 프로젝트 초기 세팅

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>리액트 세팅 완료!</Text>
       <StatusBar style="auto" />
     </View>
   );
